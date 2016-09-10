686b1d629fc9b7a0b0dbfeffc5fca4f1c09c63b7
asouza <ardsj@yahoo.com.br>
2016-09-10 03:35:00 -0300

    build structure of project

--- app/src/main/java/com/doberman/asouza/mvpdagger2/view/MainActivity.java
+++ app/src/main/java/com/doberman/asouza/mvpdagger2/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.doberman.asouza.mvpdagger2;
+package com.doberman.asouza.mvpdagger2.view;
 
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
@@ -18,6 +18,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.TextView;
+
+import com.doberman.asouza.mvpdagger2.R;
 
 public class MainActivity extends AppCompatActivity {
 
